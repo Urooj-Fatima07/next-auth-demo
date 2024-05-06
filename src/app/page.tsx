55c617f100d53d0de0e841869d536726889c2f00
hayyaFatima <155179686+hayyaFatima@users.noreply.github.com>
2024-05-06 18:22:39 +0500

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 "use client"
 import { useSession, signIn, signOut } from "next-auth/react"
 
+// Your component code
 export default function Component() {
-  const { data: session } = useSession()
-  console.log(session)
-  if(session) {
-    return <>
-      Signed in as {session.user.email} <br/>
-      <button onClick={() => signOut()}>Sign out</button>
-    </>
+  const { data: session } = useSession();
+  console.log(session);
+
+  if (session) {
+    return (
+      <>
+        Signed in as {session.user.email} <br/>
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    );
   }
-  return <>
-    Not signed in <br/>
-    <button onClick={() => signIn("github")}>Sign in using Github</button>
-    <button onClick={() => signIn("google")}>Sign in using Google</button> 
-  </>
+
+  return (
+    <>
+      Not signed in <br/>
+      <button className="custom" onClick={() => signIn("github")}>Sign in using Github</button>
+      <button className="custom" onClick={() => signIn("google")}>Sign in using Google</button> 
+    </>
+  );
 }
